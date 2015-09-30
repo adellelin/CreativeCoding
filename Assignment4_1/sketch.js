7fe9c38196e7aa359f2b3fe0657b3b5220e5a882
Adelle <Adelle@yingxis-MacBook-Air.local>
2015-09-30 00:46:20 -0400

    added notes

--- Assignment4_1/sketch.js
+++ Assignment4_1/sketch.js
@@ -1,3 +1,7 @@
+// the sketch draws a diamond one by one across the screen
+// filling each line progressively until the image is filled
+// and then starts again from the top
+
 var pos1 = 0;
 var pos2 = 5;
 var pos3 = 10;
@@ -6,48 +10,59 @@ var pos5 = 0;
 var pos6 = 5;
 var mult = 3;
 var xShift = 10;
+var x = 0;
 
 function setup() {
   createCanvas(585, 585);
   noStroke();
-  frameRate(20);
+  frameRate(40);
 }
 
 function draw() {
+  //background(255);
+  // shifts the entire drawing up to eliminate white space
+  
+  // if(mouseIsPressed == true) {
+  //   x += 1;
+
   translate(0,-15);
   scale(mult);
+  
+// continuously shift x position by 10 for 20 frame counts and then start again
   var xShift = pos3 * (frameCount % 21 - 1);
-  var yShift = pos1;
-  fill(255, random(1)*255, 255);
-  quad(pos1 + xShift, pos2 + yShift, pos2 + xShift, pos1 + yShift, pos3 + xShift,
-    pos2 + yShift, pos2 + xShift, pos3 + yShift);
+  var yShift = 0;
+  fill(255, random(1)*200, 255);
 
+// drawing the even rows of diamonds, after the first 20 frames 
+// shift the y coordinates down by 10, draw for 20 frames,
+// break for 20 frames, then shift coordinates down by 10, draw for 20 frames
+// frames are 21-40, 62-81, 103-122 etc.
+// these are odd multiples of 20, therefore j = 1, 3, 5, 7, etc.
   for (j = 1; j < 40; j += 2) {
-    fill(255, random(1)*255, 255);
+    fill(255, random(1)*200, 255);
     if (frameCount > (20 * j + (j - 1))) {
-      yShift += 10;
-      quad(pos4 + xShift, pos5 + yShift, pos5 + xShift, pos4 + yShift, pos6 + xShift,
-        pos5 + yShift, pos5 + xShift, pos6 + yShift);
+      yShift += 10; 
+      diamondDraw2();
     }
   }
-  for (i = 1; i < 60; i++) {
-    fill(255, random(1)*255, 255);
+  
+// drawing the odd rows of diamonds, same as above but during frames
+// 42-61, 83-102 etc. so we use multiples of 40 for framecount
+  for (i = 0; i < 20; i++) {
+    fill(255, random(1)*200, 255);
     if (frameCount > 41 * i + (i - 1)) {
-      var yShift = pos3 * i;
-      quad(pos1 + xShift, pos2 + yShift, pos2 + xShift, pos1 + yShift, pos3 + xShift,
-        pos2 + yShift, pos2 + xShift, pos3 + yShift);
+      var yShift = 10 * i;
+      diamondDraw();
     }
   }
+// }
+function diamondDraw() {
+   quad(pos1 + xShift, pos2 + yShift, pos2 + xShift, pos1 + yShift, pos3 + xShift,
+        pos2 + yShift, pos2 + xShift, pos3 + yShift);
+}
 
-  //   fill(random(255), random(255), random(255));
-  // if (frameCount > 62){
-  // var yShift = pos3 * 2;
-  // quad(pos4 + xShift, pos5 + yShift, pos5 + xShift, pos4 + yShift, pos6 + xShift, pos5 + yShift, pos5 + xShift, pos6 + yShift);
-  // }
-
-  // fill(random(255), random(255), random(255));
-  // if (frameCount > 83){
-  // var yShift = pos3 *2;
-  // quad(pos1 + xShift, pos2 + yShift, pos2 + xShift, pos1 + yShift, pos3 + xShift, pos2 + yShift, pos2 + xShift, pos3 + yShift);
-  // }
+function diamondDraw2() {
+  quad(pos4 + xShift, pos5 + yShift, pos5 + xShift, pos4 + yShift, pos6 + xShift,
+        pos5 + yShift, pos5 + xShift, pos6 + yShift);
+}
 }
